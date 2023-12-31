6b316af24debdaf8c376f448a5a7ce50f6b7ab75
x <x@x>
2023-12-31 13:59:26 -0500

    test

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def main():
 
     try:
         while True:
-             print(f"DEBUG: Barcode Ready: {scanner.is_barcode_ready()}")
+            print(f"DEBUG: Barcode Ready: {scanner.is_barcode_ready()}")
            # print("Scan a barcode or press 'esc' to enter commands.")
             #print("DEBUG main loop scanner.command_mode ", scanner.command_mode)
             # Check if command mode was activated before waiting for barcode
