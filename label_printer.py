4015d7fb2c2fde8a25057708d695eb60758bdfe9
Elias0419 <operryb1@gmail.com>
2024-01-13 17:33:48 -0500

    test

--- label_printer.py
+++ label_printer.py
@@ -28,6 +28,7 @@ class LabelPrinter:
         font_size = 25
         barcode_writer = ImageWriter()
 
+        # Try converting price to float and format it
         try:
             price_float = float(item_price)
             price_text = f"${price_float:.2f}"
@@ -35,42 +36,44 @@ class LabelPrinter:
             print(f"Invalid price format: {item_price}")
             return
 
-        barcode_img = upc_a(barcode_data, writer=barcode_writer).render(
-            writer_options={"module_height": 15.0}
-        )
-        barcode_img_width = label_width - 2 * margin
-        barcode_img_height = barcode_img.size[1] * (
-            barcode_img_width / barcode_img.size[0]
-        )
+        # Generate the barcode image
+        barcode_img = upc_a(barcode_data, writer=barcode_writer).render(writer_options={"module_height": 15.0})
 
-        barcode_img = barcode_img.resize(
-            (barcode_img_width, int(barcode_img_height)), Image.Resampling.LANCZOS
-        )
+        # Dynamically calculate barcode size based on label dimensions
+        max_barcode_width = label_width - 2 * margin
+        max_barcode_height = label_height - 2 * margin - font_size  # Leave space for text
+        barcode_aspect_ratio = barcode_img.size[0] / barcode_img.size[1]
+        barcode_img_width = min(max_barcode_width, max_barcode_height * barcode_aspect_ratio)
+        barcode_img_height = barcode_img_width / barcode_aspect_ratio
 
+        barcode_img = barcode_img.resize((int(barcode_img_width), int(barcode_img_height)), Image.Resampling.LANCZOS)
+
+        # Create the label image
         label_image = Image.new("L", (label_width, label_height), color=255)
         draw = ImageDraw.Draw(label_image)
         font = ImageFont.truetype(font_path, font_size)
 
+        # Calculate text positioning
         price_text_width = draw.textlength(price_text, font=font)
         price_text_height = font.getmetrics()[0]
-
         price_x_position = (label_width - price_text_width) // 2
         price_y_position = margin
 
-        barcode_x_position = (label_width - barcode_img_width) // 2
-        barcode_y_position = (
-            price_y_position + price_text_height + margin
-        )
+        # Calculate barcode positioning
+        barcode_x_position = (label_width - int(barcode_img_width)) // 2
+        barcode_y_position = price_y_position + price_text_height + margin
 
+        # Draw text and paste barcode
         draw.text((price_x_position, price_y_position), price_text, font=font, fill=0)
         label_image.paste(barcode_img, (barcode_x_position, barcode_y_position))
 
-        save_path = "test.png"
-        label_image.save(save_path)
+        # Save and print the label
+        #label_image.save(save_path)
         qlr = brother_ql.BrotherQLRaster('QL-710W')
         qlr.exception_on_warning = True
         convert(qlr=qlr, images=[label_image], label='23x23')
         send(instructions=qlr.data, printer_identifier='usb://0x04F9:0x2043', backend_identifier='pyusb')
+
 
     def process_queue(self):
         print("we are inside process queue")
