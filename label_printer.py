64e134f1d6133151f7950fde6a1ffd0e3b4acd90
Elias0419 <operryb1@gmail.com>
2024-01-13 18:28:34 -0500

    test

--- label_printer.py
+++ label_printer.py
@@ -39,15 +39,23 @@ class LabelPrinter:
         font = ImageFont.truetype(font_path, font_size)
         price_text = f"${float(item_price):.2f}"
         text_width = draw.textlength(price_text, font=font)
-        # Estimate text height as 1.2 times the font size
         text_height = 1.2 * font_size
         text_position = ((label_width - text_width) / 2, margin)
         draw.text(text_position, price_text, font=font, fill="black")
 
         # Place barcode
-        barcode_height = label_height * 3 // 4 - 2 * margin
-        barcode_img = barcode_img.resize((label_width - 2 * margin, barcode_height))
-        label_image.paste(barcode_img, (margin, label_height // 4))
+        barcode_target_width = label_width - 2 * margin
+        # Calculate new height while maintaining aspect ratio
+        barcode_aspect_ratio = barcode_img.height / barcode_img.width
+        barcode_target_height = int(barcode_target_width * barcode_aspect_ratio)
+
+        # Ensure barcode height does not exceed label height limit
+        barcode_target_height = min(barcode_target_height, label_height * 3 // 4 - 2 * margin)
+
+        barcode_img = barcode_img.resize((barcode_target_width, barcode_target_height))
+        barcode_position_y = label_height // 4 + (label_height * 3 // 4 - barcode_target_height) // 2
+        label_image.paste(barcode_img, (margin, barcode_position_y))
+
 
         # Save the image
         label_image.save(save_path)
