50c8e21b696016a082fde338227cf124c5d1cb6b
Elias0419 <operryb1@gmail.com>
2024-01-13 18:35:18 -0500

    test

--- label_printer.py
+++ label_printer.py
@@ -22,16 +22,22 @@ class LabelPrinter:
         )
 
     def print_barcode_label(self, barcode_data, item_price, save_path):
+
         label_width, label_height = 202, 202
         margin = 10
         font_path = "/usr/share/fonts/TTF/Hack-Regular.ttf"
         font_size = 25
-        barcode_writer = ImageWriter()
 
-        # Generate barcode
-        barcode_img = upc_a(barcode_data, writer=barcode_writer).render()
+        # Create barcode in SVG format
+        svg_io = BytesIO()
+        barcode_class = barcode.get_barcode_class('ean13')  # Replace 'ean13' with your desired format
+        barcode_object = barcode_class(barcode_data, writer=SVGWriter())
+        barcode_object.write(svg_io)
 
-        # Create an empty image
+        # Convert SVG to PNG using CairoSVG
+        png_data = cairosvg.svg2png(bytestring=svg_io.getvalue())
+
+        # Create an empty image for the label
         label_image = Image.new('RGB', (label_width, label_height), color='white')
         draw = ImageDraw.Draw(label_image)
 
@@ -43,18 +49,14 @@ class LabelPrinter:
         text_position = ((label_width - text_width) / 2, margin)
         draw.text(text_position, price_text, font=font, fill="black")
 
-        # Place barcode
+        # Place barcode (converted from SVG to PNG)
+        barcode_img = Image.open(BytesIO(png_data))
         barcode_target_width = label_width - 2 * margin
-        # Calculate new height while maintaining aspect ratio
-        barcode_aspect_ratio = barcode_img.height / barcode_img.width
-        barcode_target_height = int(barcode_target_width * barcode_aspect_ratio)
-
-        # Ensure barcode height does not exceed label height limit
-        barcode_target_height = min(barcode_target_height, label_height * 3 // 4 - 2 * margin)
-
-        barcode_img = barcode_img.resize((barcode_target_width, barcode_target_height))
+        barcode_target_height = label_height // 2
+        barcode_img = barcode_img.resize((barcode_target_width, barcode_target_height), Image.ANTIALIAS)
         barcode_position_y = label_height // 4 + (label_height * 3 // 4 - barcode_target_height) // 2
         label_image.paste(barcode_img, (margin, barcode_position_y))
+
 
 
         # Save the image
