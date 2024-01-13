b31c4a7e9004eb56d3afdfe0d3c228c855b07c01
Elias0419 <operryb1@gmail.com>
2024-01-12 23:22:32 -0500

    test

---
+++ wrapper.py
@@ -0,0 +1,14 @@
+import subprocess
+
+def run_app():
+    script_path = 'kivymd_main_testing.py'
+
+    while True:
+
+        result = subprocess.run(['../0/bin/python', script_path])
+
+        if result.returncode != 1:
+            break
+
+if __name__ == "__main__":
+    run_app()
