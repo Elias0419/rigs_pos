eea85496d4ad97b875d9ff9f591f40b9e1880f4a
Elias0419 <operryb1@gmail.com>
2024-02-10 16:12:23 -0500

    test

--- main.py
+++ main.py
@@ -113,8 +113,8 @@ class CashRegisterApp(MDApp):
         )
         btn_tools = self.utilities.create_md_raised_button(
             "Tools",
-            lambda x: self.popup_manager.show_lock_screen(),
-            #self.button_handler.on_button_press,  #################
+            #lambda x: self.popup_manager.show_lock_screen(),
+            self.button_handler.on_button_press,  #################
             # lambda x: self.show_add_or_bypass_popup("210973141121"),
             #lambda x: self.popup_manager.show_add_or_bypass_popup("220973141121"),
             # lambda x: self.open_category_button_popup(),
