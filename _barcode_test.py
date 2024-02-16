93b9942c206ca89880b4fbafcb34a695413d1682
Elias0419 <operryb1@gmail.com>
2024-02-16 12:34:23 -0500

    experimental barcode scanner

---
+++ _barcode_test.py
@@ -0,0 +1,176 @@
+import threading
+import usb.core
+import usb.util
+
+
+class BarcodeScanner:
+    def __init__(self, ref):
+        self.app = ref
+        self.current_barcode = ""
+
+        self.idVendor = 0x28E9
+        self.idProduct = 0x03DA
+
+        try:
+            self.device = self.initializeUSBDevice()
+        except ValueError as e:
+            print(e)  # TODO do something
+
+        self.barcode_ready = threading.Event()
+        self.stop_thread = threading.Event()
+        self.thread = threading.Thread(target=self.capture_raw_data, daemon=True)
+        self.thread.start()
+
+    def initializeUSBDevice(self):
+
+        device = usb.core.find(idVendor=self.idVendor, idProduct=self.idProduct)
+        if device is None:
+            raise ValueError("[Barcode Scanner]: Device Not Found")
+
+        if device.is_kernel_driver_active(0):
+            device.detach_kernel_driver(0)
+
+        device.set_configuration()
+        configuration = device.get_active_configuration()
+        interface = configuration[(0, 0)]
+
+        self.endpoint = usb.util.find_descriptor(
+            interface,
+            custom_match=lambda e: usb.util.endpoint_direction(e.bEndpointAddress)
+            == usb.util.ENDPOINT_IN,
+        )
+
+        if self.endpoint is None:
+            raise ValueError("[Barcode Scanner: Endpoint not found]")
+
+        return device
+
+    def capture_raw_data(self):
+
+        conversion_table = {
+            0: ["", ""],
+            4: ["a", "A"],
+            5: ["b", "B"],
+            6: ["c", "C"],
+            7: ["d", "D"],
+            8: ["e", "E"],
+            9: ["f", "F"],
+            10: ["g", "G"],
+            11: ["h", "H"],
+            12: ["i", "I"],
+            13: ["j", "J"],
+            14: ["k", "K"],
+            15: ["l", "L"],
+            16: ["m", "M"],
+            17: ["n", "N"],
+            18: ["o", "O"],
+            19: ["p", "P"],
+            20: ["q", "Q"],
+            21: ["r", "R"],
+            22: ["s", "S"],
+            23: ["t", "T"],
+            24: ["u", "U"],
+            25: ["v", "V"],
+            26: ["w", "W"],
+            27: ["x", "X"],
+            28: ["y", "Y"],
+            29: ["z", "Z"],
+            30: ["1", "!"],
+            31: ["2", "@"],
+            32: ["3", "#"],
+            33: ["4", "$"],
+            34: ["5", "%"],
+            35: ["6", "^"],
+            36: ["7", "&"],
+            37: ["8", "*"],
+            38: ["9", "("],
+            39: ["0", ")"],
+            40: ["\n", "\n"],
+            41: ["\x1b", "\x1b"],
+            42: ["\b", "\b"],
+            43: ["\t", "\t"],
+            44: [" ", " "],
+            45: ["_", "_"],
+            46: ["=", "+"],
+            47: ["[", "{"],
+            48: ["]", "}"],
+            49: ["\\", "|"],
+            50: ["#", "~"],
+            51: [";", ":"],
+            52: ["'", '"'],
+            53: ["`", "~"],
+            54: [",", "<"],
+            55: [".", ">"],
+            56: ["/", "?"],
+            100: ["\\", "|"],
+            103: ["=", "="],
+        }
+
+        while not self.stop_thread.is_set():
+            try:
+                data = self.device.read(
+                    self.endpoint.bEndpointAddress,
+                    self.endpoint.wMaxPacketSize,
+                    timeout=5000,
+                )
+                if data[2] != 0:
+                    character = conversion_table.get(data[2], [""])[0]
+                    if character == "\n":
+
+                        self.barcode_ready.set()
+
+                    else:
+                        self.current_barcode += character
+            except usb.core.USBError as e:
+                if e.errno == 110:
+                    continue
+                else:
+                    raise
+
+    def is_barcode_ready(self):
+        return self.barcode_ready.is_set()
+
+    def check_for_scanned_barcode(self, dt):
+        if self.is_barcode_ready():
+            barcode = self.current_barcode
+            self.handle_global_barcode_scan(barcode)
+            self.current_barcode = ""
+            self.barcode_ready.clear()
+
+    def handle_global_barcode_scan(self, barcode):
+
+        if self.app.current_context == "inventory":
+            self.app.inventory_manager.handle_scanned_barcode(barcode)
+        elif self.app.current_context == "label":
+            self.app.label_manager.handle_scanned_barcode(barcode)
+        elif self.app.current_context == "inventory_item":
+            self.app.inventory_manager.handle_scanned_barcode_item(barcode)
+
+        else:
+            self.handle_scanned_barcode(barcode)
+
+    def handle_scanned_barcode(self, barcode):
+
+        try:
+            if "-" in barcode and any(c.isalpha() for c in barcode):
+                self.app.history_manager.display_order_details_from_barcode_scan(
+                    barcode
+                )
+            else:
+                item_details = self.app.db_manager.get_item_details(barcode)
+
+                if item_details:
+                    item_name, item_price = item_details
+                    self.app.order_manager.add_item(item_name, item_price)
+                    self.app.utilities.update_display()
+                    self.app.utilities.update_financial_summary()
+                    return item_details
+                else:
+                    self.app.popup_manager.show_add_or_bypass_popup(barcode)
+        except Exception as e:
+            print(f"Exception in handle_scanned_barcode\n{e}")
+
+    def close(self):
+        self.stop_thread.set()
+        if self.device.is_kernel_driver_active(0):
+            self.device.attach_kernel_driver(0)
