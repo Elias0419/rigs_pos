20318ef0b078857922af2ee4995110563928775e
Elias0419 <operryb1@gmail.com>
2024-02-17 14:42:10 -0500

    remove ast and partial deps

--- history_manager.py
+++ history_manager.py
@@ -1,31 +1,26 @@
-import ast
 import csv
 import json
 from datetime import datetime, timedelta
-from functools import partial
-from kivy.clock import Clock
 
-from kivymd.app import MDApp
+from kivy.clock import Clock
 from kivy.metrics import dp
-from kivymd.uix.boxlayout import BoxLayout
-from kivymd.uix.gridlayout import GridLayout
-from kivymd.uix.recycleview import RecycleView
-from kivy.uix.recycleboxlayout import RecycleBoxLayout
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
 from kivy.properties import StringProperty, ObjectProperty
-from kivymd.uix.pickers import MDDatePicker
-from kivymd.uix.button import MDRaisedButton
+from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+
+from kivymd.uix.boxlayout import BoxLayout
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.pickers import MDDatePicker
 
 from database_manager import DatabaseManager
 from receipt_printer import ReceiptPrinter
 
 
+
 class NullableStringProperty(StringProperty):
     def __init__(self, *args, **kwargs):
 
-        kwargs.setdefault('allownone', True)
+        kwargs.setdefault("allownone", True)
         super(NullableStringProperty, self).__init__(*args, **kwargs)
 
 
@@ -33,7 +28,8 @@ class HistoryPopup(Popup):
     def __init__(self, **kwargs):
         super(HistoryPopup, self).__init__(**kwargs)
         self.db_manager = DatabaseManager("inventory.db", self)
-        #self.history_view = HistoryView()
+        # self.history_view = HistoryView()
+
     def show_hist_reporting_popup(self):
         order_history = self.db_manager.get_order_history()
         # print(order_history)
@@ -94,7 +90,6 @@ class HistoryView(BoxLayout):
 
             Clock.schedule_once(self.init_filter, 0.1)
 
-
     def initialize_buttons(self):
         self.button_layout = BoxLayout(orientation="horizontal", size_hint=(1, 0.2))
         self.button_layout.add_widget(
@@ -133,7 +128,6 @@ class HistoryView(BoxLayout):
         )
         return self.button_layout
 
-
     def init_filter(self, dt):
         self.filter_today()
         self.update_totals()
@@ -144,29 +138,29 @@ class HistoryView(BoxLayout):
         total_tax = sum(float(order["tax"]) for order in self.rv_data)
         total_with_tax = sum(float(order["total_with_tax"]) for order in self.rv_data)
 
-        self.total_amount_label.text = f"${total_amount:.2f} + ${total_tax:.2f} tax = ${total_with_tax:.2f}"
-
-
+        self.total_amount_label.text = (
+            f"${total_amount:.2f} + ${total_tax:.2f} tax = ${total_with_tax:.2f}"
+        )
 
     def show_reporting_popup(self, order_history):
         self.order_history = order_history
         try:
             self.rv_data = [
-            {
-                "order_id": order[0],
-                "items": self.format_items(order[1]),
-                "total": self.format_money(order[2]),
-                "tax": self.format_money(order[3]),
-                "discount": self.format_money(order[4]),
-                "total_with_tax": self.format_money(order[5]),
-                "timestamp": self.format_date(order[6]),
-                "payment_method": order[7],
-                "amount_tendered": self.format_money(order[8]),
-                "change_given": self.format_money(order[9]),
-                "history_view": self,
-            }
-            for order in order_history
-        ]
+                {
+                    "order_id": order[0],
+                    "items": self.format_items(order[1]),
+                    "total": self.format_money(order[2]),
+                    "tax": self.format_money(order[3]),
+                    "discount": self.format_money(order[4]),
+                    "total_with_tax": self.format_money(order[5]),
+                    "timestamp": self.format_date(order[6]),
+                    "payment_method": order[7],
+                    "amount_tendered": self.format_money(order[8]),
+                    "change_given": self.format_money(order[9]),
+                    "history_view": self,
+                }
+                for order in order_history
+            ]
 
             self.rv_data.reverse()
             self.ids.history_rv.data = self.rv_data
@@ -191,7 +185,6 @@ class HistoryView(BoxLayout):
             return history_row
         except Exception as e:
             print(f"[HistoryManager] create_history_row\n{e}")
-
 
     def show_specific_day_popup(self, instance):
         specific_day_picker = MDDatePicker()
@@ -338,35 +331,34 @@ class HistoryView(BoxLayout):
                 popup = OrderDetailsPopup(specific_order, self.receipt_printer)
                 popup.open()
             else:
-                self.order_not_found_popup(barcode_str) # needs testing
+                self.order_not_found_popup(barcode_str)  # needs testing
 
         except Exception as e:
             print(f"[HistoryManager] display_order_details_from_barcode_scan\n{e}")
 
     def order_not_found_popup(self, order):
-        not_found_layout = BoxLayout(size_hint=(1,1))
+        not_found_layout = BoxLayout(size_hint=(1, 1))
         not_found_label = Label(text=f"Order {order} Not Found")
-        not_found_button = MDRaisedButton(text="Dismiss", on_press=lambda x: self.not_found_popup.dismiss())
+        not_found_button = MDRaisedButton(
+            text="Dismiss", on_press=lambda x: self.not_found_popup.dismiss()
+        )
         not_found_layout.add_widget(not_found_label)
         not_found_layout.add_widget(not_found_button)
-        self.not_found_popup = Popup(content=not_found_layout, size_hint=(0.4,0.4))
+        self.not_found_popup = Popup(content=not_found_layout, size_hint=(0.4, 0.4))
         self.not_found_popup.open()
-
 
     def show_order_details(self, order_id):
         specific_order = next(
             (order for order in self.order_history if order[0] == order_id), None
         )
         if specific_order:
-            #print(specific_order)
+            # print(specific_order)
             self.clear_widgets()
             try:
                 history_row = self.create_history_row(specific_order)
                 self.add_widget(history_row)
             except Exception as e:
                 print(f"[HistoryManager] show_order_details\n{e}")
-
-
 
     def format_items(self, items_str):
         try:
@@ -378,13 +370,14 @@ class HistoryView(BoxLayout):
             else:
                 items_list = parsed_data
 
-            all_item_names = ", ".join(item.get("name", "Unknown") for item in items_list)
+            all_item_names = ", ".join(
+                item.get("name", "Unknown") for item in items_list
+            )
 
             return self.truncate_text(all_item_names)
         except json.JSONDecodeError as e:
             print(f"JSON parsing error in format_items: {e}")
             return self.truncate_text("Error parsing items")
-
 
     def format_money(self, value):
         formatted_value = "{:.2f}".format(value)
@@ -408,7 +401,18 @@ class HistoryView(BoxLayout):
         with open(filename, "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
             writer.writerow(
-                ["Order ID", "Items", "Total", "Tax", "Discount", "Total with Tax", "Timestamp", "Payment Method", "Amount Tendered", "Change Given"]
+                [
+                    "Order ID",
+                    "Items",
+                    "Total",
+                    "Tax",
+                    "Discount",
+                    "Total with Tax",
+                    "Timestamp",
+                    "Payment Method",
+                    "Amount Tendered",
+                    "Change Given",
+                ]
             )
             for row in csv_data:
                 writer.writerow(row)
@@ -440,7 +444,7 @@ class HistoryView(BoxLayout):
                 order["timestamp"],
                 order["payment_method"],
                 order["amount_tendered"],
-                order["change_given"]
+                order["change_given"],
             ]
             for order in self.rv_data
         ]
@@ -464,7 +468,7 @@ class OrderDetailsPopup(Popup):
 
         button_layout.add_widget(
             MDRaisedButton(
-                text="Print Receipt", on_press=partial(self.print_receipt, order=order)
+                text="Print Receipt", on_press=lambda instance: self.print_receipt(order=order)
             )
         )
         button_layout.add_widget(MDRaisedButton(text="Refund", on_press=self.refund))
@@ -495,21 +499,35 @@ class OrderDetailsPopup(Popup):
             else:
                 items_list = parsed_data
 
-            all_item_names = ", ".join(item.get("name", "Unknown") for item in items_list)
+            all_item_names = ", ".join(
+                item.get("name", "Unknown") for item in items_list
+            )
 
             return all_item_names
         except json.JSONDecodeError as e:
             print(f"JSON parsing error in format_items: {e}")
             return "Error parsing items"
 
-    def convert_order_to_dict(self, order_tuple):
-        #print(order_tuple)
-        order_id, items_json, total, tax, discount, total_with_tax, timestamp, payment_method, amount_tendered, change_given = order_tuple
+    def convert_order_to_dict(self, order):
+        print(order)
+
+        (
+            order_id,
+            items_json,
+            total,
+            tax,
+            discount,
+            total_with_tax,
+            timestamp,
+            payment_method,
+            amount_tendered,
+            change_given,
+        ) = order
         try:
             items = json.loads(items_json)
         except json.JSONDecodeError as e:
-            print(f"\n\n\n\n\n\n\n\json error \n{e}")
-            items = ast.literal_eval(items_json)
+            print(f"[OrderDetailsPopup] convert_order_to_dict \n{e}")
+            #items = ast.literal_eval(items_json)
 
         if isinstance(items, list):
             items_dict = {str(i): item for i, item in enumerate(items)}
@@ -529,7 +547,7 @@ class OrderDetailsPopup(Popup):
         return order_dict
 
     def format_order_details(self, order):
-        #print(order)
+        # print(order)
         formatted_order = [
             f"Order ID: {order[0]}",
             f"Items: {self.format_items(order[1])}",
@@ -539,9 +557,11 @@ class OrderDetailsPopup(Popup):
             f"Total with Tax: ${self.history_view.format_money(order[5])}",
             f"Timestamp: {self.history_view.format_date(order[6])}",
         ]
-        formatted_order.extend([
-            f"Payment Method: {order[7]}",
-            f"Amount Tendered: ${self.history_view.format_money(order[8])}",
-            f"Change Given: ${self.history_view.format_money(order[9])}",
-        ])
+        formatted_order.extend(
+            [
+                f"Payment Method: {order[7]}",
+                f"Amount Tendered: ${self.history_view.format_money(order[8])}",
+                f"Change Given: ${self.history_view.format_money(order[9])}",
+            ]
+        )
         return "\n".join(formatted_order) if formatted_order is not None else ""
