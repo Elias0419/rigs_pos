da7b87b658f4d2a1a84336f89709f41fe899cb57
Elias0419 <operryb1@gmail.com>
2024-03-03 14:23:42 -0500

    debug

--- wrapper.py
+++ wrapper.py
@@ -44,8 +44,10 @@ class Wrapper():
 
                     # self.set_emergency_reboot_flag()
                     try:
-                        fallback_command = ['nohup', '/home/rigs/1/bin/python', '/home/rigs/fallback_rigs_pos/main.py']
-                        fallback_process = subprocess.Popen(fallback_command, start_new_session=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                        fallback_command = ['/home/rigs/1/bin/python', '/home/rigs/fallback_rigs_pos/main.py']
+                        fallback_process = subprocess.Popen(['nohup', '/home/rigs/1/bin/python', '/home/rigs/fallback_rigs_pos/main.py'],
+                                    stdout=open('fallback_stdout.log', 'w'),
+                                    stderr=open('fallback_stderr.log', 'w'))
                         time.sleep(5)
                         if fallback_process.poll() is None:
                             self.send_email("Fallback successful", "The application has successfully fallen back.", recipient)
