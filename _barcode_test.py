a43cb2676e84cae4a8dbf45bf339cc4b91bfcb6e
Elias0419 <operryb1@gmail.com>
2024-03-24 17:16:41 -0400

    debug

--- _barcode_test.py
+++ _barcode_test.py
@@ -165,7 +165,7 @@ class BarcodeScanner:
             self.handle_scanned_barcode(barcode)
 
     def handle_scanned_barcode(self, barcode):
-        try:
+        #try:
             if "-" in barcode and any(c.isalpha() for c in barcode):
                 self.app.history_manager.display_order_details_from_barcode_scan(
                     barcode
@@ -216,8 +216,8 @@ class BarcodeScanner:
             if not found:
                 self.app.popup_manager.show_add_or_bypass_popup(barcode)
 
-        except Exception as e:
-            print(f"Exception in handle_scanned_barcode\n{e}")
+        # except Exception as e:
+        #     print(f"Exception in handle_scanned_barcode\n{e}")
 
     def process_item_details(self, item_details):
         item_name = item_details.get('name', 'Error!')
