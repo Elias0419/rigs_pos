231b8c96817bf375a0f1f831f5fc6ce96cdf3fc1
Elias0419 <operryb1@gmail.com>
2024-03-29 12:01:00 -0400

    disable escape to exit

--- main.py
+++ main.py
@@ -6,19 +6,21 @@
 
 
 from kivy.config import Config
-#Config.set('kivy', 'keyboard_mode', 'systemanddock')
-#Config.set('input', 'isolution multitouch', 'hidinput,/dev/input/event12')
-Config.set('graphics', 'show_cursor', '0')
+
+# Config.set('kivy', 'keyboard_mode', 'systemanddock')
+# Config.set('input', 'isolution multitouch', 'hidinput,/dev/input/event12')
+# Config.set('graphics', 'show_cursor', '0')
 Config.set("graphics", "multisamples", "8")
-Config.set('graphics', 'kivy_clock', 'interrupt')
+Config.set("graphics", "kivy_clock", "interrupt")
+Config.set('kivy', 'exit_on_escape', '0')
+
 from kivy.core.window import Window
-from kivy.modules import monitor, inspector
+
+# from kivy.modules import monitor, inspector
 from kivymd.app import MDApp
 
 
 from util import Utilities
-
-
 
 
 Window.maximize()
@@ -28,26 +30,16 @@ Window.borderless = True
 class CashRegisterApp(MDApp):
     def __init__(self, **kwargs):
         super(CashRegisterApp, self).__init__(**kwargs)
-
+        self.utilities = Utilities(self)
 
     def on_start(self):
         self.utilities.initialize_global_variables()
         self.utilities.load_settings()
 
-
     def build(self):
-        self.utilities = Utilities(self)
-        self.dual_pane_mode = False
-        self.utilities.check_dual_pane_mode()
-        if self.dual_pane_mode:
-            self.utilities.instantiate_modules(module_name=None, dual_pane_mode=True) ##
-            layout = self.utilities.create_main_layout(dual_pane_mode=True)
-        else:
-            self.utilities.instantiate_modules()
-            layout = self.utilities.create_main_layout()
+        self.utilities.instantiate_modules()
+        layout = self.utilities.create_main_layout()
         return layout
-
-
 
 
 if __name__ == "__main__":
