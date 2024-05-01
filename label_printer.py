3fac33c0e5ce0a5c922bceb1b34c805d6173909a
Elias0419 <operryb1@gmail.com>
2024-05-01 16:59:37 -0400

    debug

--- label_printer.py
+++ label_printer.py
@@ -612,8 +612,8 @@ class LabelPrinter:
 
         barcode_image = upc.render(
             {
-                "module_width": 0.17,
-                "module_height": 10 if not include_text else 8,
+                # "module_width": 0.17,
+                # "module_height": 10 if not include_text else 8,
                 # "font_size": 2,
                 # "dpi": 300,
                 # "write_text": False,
