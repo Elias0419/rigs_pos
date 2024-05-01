5b3a1534e7bd68a657d6a28c683f599651f8543a
Elias0419 <operryb1@gmail.com>
2024-05-01 17:05:06 -0400

    debug

--- label_printer.py
+++ label_printer.py
@@ -612,15 +612,15 @@ class LabelPrinter:
 
         barcode_image = upc.render(
             {
-                # "module_width": 0.17,
-                # "module_height": 10 if not include_text else 8,
+                "module_width": 0.17,
+                "module_height": 10 if not include_text else 8,
                 # "font_size": 2,
                 # "dpi": 300,
                 # "write_text": False,
             }
         )
         self.debug_dimensions(barcode_image)
-        barcode_image = barcode_image.resize((300, 221), Image.Resampling.LANCZOS)
+        barcode_image = barcode_image.resize((220, 220), Image.Resampling.LANCZOS)
         self.debug_dimensions(barcode_image)
 
         label_image = Image.new("RGB", (label_width, label_height), "white")
