02fd21f8985a685dd46a466fa422f3db5a4037b7
Elias0419 <operryb1@gmail.com>
2024-05-19 11:08:44 -0400

    debug

--- receipt_printer.py
+++ receipt_printer.py
@@ -39,89 +39,89 @@ class ReceiptPrinter:
         except Exception as e:
             print("Error loading logo:", e)
 
-        try:
-            self.printer.image(logo, (200, -60))
-            date = str(datetime.now().replace(microsecond=0))
-            self.printer.set(align="center", font="a")
-            self.printer.textln()
-            self.printer.textln("402C Main St, Wakefield, RI")
-            self.printer.textln("401-363-9866")
-            self.printer.textln()
-            self.printer.textln()
+        # try:
+        self.printer.image(logo, (200, -60))
+        date = str(datetime.now().replace(microsecond=0))
+        self.printer.set(align="center", font="a")
+        self.printer.textln()
+        self.printer.textln("402C Main St, Wakefield, RI")
+        self.printer.textln("401-363-9866")
+        self.printer.textln()
+        self.printer.textln()
 
-            max_line_width = 48
-            self.printer.set(align="left", font="a", bold=False)
+        max_line_width = 48
+        self.printer.set(align="left", font="a", bold=False)
 
-            for item in order_details["items"].values():
-                if item["quantity"] > 1:
-                    item_name = f"{item['name']} x{item['quantity']}"
-                else:
-                    item_name = item["name"]
-                price = f"${item['total_price']:.2f}"
-                spaces = " " * (max_line_width - len(item_name) - len(price))
-                item_line = item_name + spaces + price
+        for item in order_details["items"].values():
+            if item["quantity"] > 1:
+                item_name = f"{item['name']} x{item['quantity']}"
+            else:
+                item_name = item["name"]
+            price = f"${item['total_price']:.2f}"
+            spaces = " " * (max_line_width - len(item_name) - len(price))
+            item_line = item_name + spaces + price
 
-                self.printer.textln(item_line)
-                if float(item["discount"]["amount"]) > 0:
-                    discount_amount = float(item["discount"]["amount"])
-                    discount_text = f"Discount: -${discount_amount:.2f}"
-                    spaces = " " * (max_line_width - len(discount_text))
-                    self.printer.textln(spaces + discount_text)
+            self.printer.textln(item_line)
+            if float(item["discount"]["amount"]) > 0:
+                discount_amount = float(item["discount"]["amount"])
+                discount_text = f"Discount: -${discount_amount:.2f}"
+                spaces = " " * (max_line_width - len(discount_text))
+                self.printer.textln(spaces + discount_text)
 
-                self.printer.textln()
-
-            self.printer.set(align="right", font="a", bold=True)
             self.printer.textln()
 
-            self.printer.textln(f"Subtotal: ${order_details['subtotal']:.2f}")
+        self.printer.set(align="right", font="a", bold=True)
+        self.printer.textln()
 
-            if order_details["discount"] > 0:
-                self.printer.textln(f"Discount: -${order_details['discount']:.2f}")
+        self.printer.textln(f"Subtotal: ${order_details['subtotal']:.2f}")
 
-            self.printer.textln(f"Tax: ${order_details['tax_amount']:.2f}")
+        if order_details["discount"] > 0:
+            self.printer.textln(f"Discount: -${order_details['discount']:.2f}")
 
-            self.printer.textln(f"Total: ${order_details['total_with_tax']:.2f}")
-            if not draft:
-                if order_details["payment_method"] == "Cash":
-                    self.printer.textln(
-                        f"Cash: ${order_details['amount_tendered']:.2f}"
-                    )
-                    self.printer.textln(f"Change: ${order_details['change_given']:.2f}")
+        self.printer.textln(f"Tax: ${order_details['tax_amount']:.2f}")
 
-                elif order_details["payment_method"] == "Split":
-                    self.printer.textln("Split Payment")
-                elif order_details["payment_method"] == "Debit":
-                    self.printer.textln("Debit Payment")
-                else:
-                    self.printer.textln("Credit Payment")
+        self.printer.textln(f"Total: ${order_details['total_with_tax']:.2f}")
+        if not draft:
+            if order_details["payment_method"] == "Cash":
+                self.printer.textln(
+                    f"Cash: ${order_details['amount_tendered']:.2f}"
+                )
+                self.printer.textln(f"Change: ${order_details['change_given']:.2f}")
 
-            self.printer.set(align="center", font="b", bold=False)
+            elif order_details["payment_method"] == "Split":
+                self.printer.textln("Split Payment")
+            elif order_details["payment_method"] == "Debit":
+                self.printer.textln("Debit Payment")
+            else:
+                self.printer.textln("Credit Payment")
+
+        self.printer.set(align="center", font="b", bold=False)
+        self.printer.textln()
+
+        barcode_data = self.uuid_to_decimal_string(str(order_details["order_id"]))
+        self.printer.textln(date)
+        self.printer.textln(order_details["order_id"])
+        if not draft:
+            try:
+                self.printer.barcode(barcode_data, "CODE128", width=64, height=3, pos="OFF", font="A", function_type="B")
+            except Exception as e:
+                self.app.popup_manager.catch_receipt_printer_errors(e, order_details)
+                return False
+
+        if reprint:
+            self.printer.set(align="center", font="a", bold=True)
             self.printer.textln()
+            self.printer.textln("Copy")
+        if draft:
+            self.printer.set(align="center", font="a", bold=True)
+            self.printer.textln()
+            self.printer.textln("UNPAID")
 
-            barcode_data = self.uuid_to_decimal_string(str(order_details["order_id"]))
-            self.printer.textln(date)
-            self.printer.textln(order_details["order_id"])
-            if not draft:
-                try:
-                    self.printer.barcode(barcode_data, "CODE128", width=64, height=3, pos="OFF", font="A", function_type="B")
-                except Exception as e:
-                    self.app.popup_manager.catch_receipt_printer_errors(e, order_details)
-                    return False
-
-            if reprint:
-                self.printer.set(align="center", font="a", bold=True)
-                self.printer.textln()
-                self.printer.textln("Copy")
-            if draft:
-                self.printer.set(align="center", font="a", bold=True)
-                self.printer.textln()
-                self.printer.textln("UNPAID")
-
-            self.printer.cut()
-            return True
-        except Exception as e:
-            self.app.popup_manager.catch_receipt_printer_errors(e, order_details)
-            return False
+        self.printer.cut()
+        return True
+        # except Exception as e:
+        #     self.app.popup_manager.catch_receipt_printer_errors(e, order_details)
+        #     return False
 
 
     def close(self):
