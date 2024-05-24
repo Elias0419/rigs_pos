c3bacbbc3a658913086fb36524bce5060e6e20be
Elias0419 <operryb1@gmail.com>
2024-05-24 11:22:52 -0400

    test

--- util.py
+++ util.py
@@ -806,7 +806,7 @@ class Utilities:
 
         self.base_layout = FloatLayout()
         try:
-            bg_image = Image(source="images/grey_mountains.jpg", fit_mode="fill")
+            bg_image = Image(source="images/test.webp", fit_mode="fill")
             self.base_layout.add_widget(bg_image)
 
         except Exception as e:
