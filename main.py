ec4839fd6293c5633a1379729bfcb1c89f7ec7bb
Elias0419 <operryb1@gmail.com>
2025-01-25 12:05:16 -0500

    sync window settings

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from kivy.config import Config
-Config.set('graphics', 'width', '1902')
+Config.set('graphics', 'width', '1920')
 Config.set('graphics', 'height', '1080')
 Config.set('graphics', 'show_cursor', '0')
 Config.set("graphics", "multisamples", "8")
