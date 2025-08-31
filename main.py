8281e57f049cdd17a000bc19b9d34b847815f665
Elias0419 <operryb1@gmail.com>
2025-08-31 14:58:47 -0400

    clean up is_rigs and move font init

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.config import Config
 Config.set('graphics', 'width', '1920')
 Config.set('graphics', 'height', '1080')
-Config.set('graphics', 'show_cursor', '0')
+#Config.set('graphics', 'show_cursor', '0')
 Config.set("graphics", "multisamples", "8")
 Config.set("graphics", "kivy_clock", "interrupt")
 Config.set("kivy", "exit_on_escape", "0")
@@ -9,15 +9,10 @@ Config.set("kivy", "exit_on_escape", "0")
 from kivymd.app import MDApp
 from kivy.core.window import Window
 
-from kivy.core.text import LabelBase
-LabelBase.register(
-    name="Emoji",
-    fn_regular="images/seguiemj.ttf"
-)
-
 from util import setup_logging, Utilities
 setup_logging()
 import logging
+
 logger = logging.getLogger('rigs_pos')
 
 class CashRegisterApp(MDApp):
@@ -25,7 +20,8 @@ class CashRegisterApp(MDApp):
         super(CashRegisterApp, self).__init__(**kwargs)
         self.utilities = Utilities(self)
         self.logged_in_user = "nobody"
-        if self.utilities.is_rigs():
+        self.is_rigs = self.utilities.is_rigs()
+        if self.is_rigs:
             self.admin = False
         else:
             self.admin = True
@@ -37,6 +33,7 @@ class CashRegisterApp(MDApp):
 
     def build(self):
         self.utilities.instantiate_modules()
+        self.utilities.register_fonts()
         layout = self.utilities.create_main_layout()
         return layout
 
